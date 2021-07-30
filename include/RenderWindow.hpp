5cd8e48d99e9e2dc6dc8d07f888cb6757dca4911
Raahim Fareed <raahim1965@hotmail.com>
2021-07-31 00:34:36 +0500

    Added functions to render entity, texture, world and text

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -1,15 +1,23 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "World.hpp"
+#include "Entity.hpp"
 
 class RenderWindow
 {
 public:
-    RenderWindow() = default;
-
+    RenderWindow();
     RenderWindow(const char* cpTitle, const int cWidth, const int cHeight, SDL_Color backgroundColor = {0, 0, 0});
     void create(const char* pTitle, const int cWidth, const int cHeight, SDL_Color backgroundColor = {0, 0, 0});
     SDL_Texture* pLoadTexture(const char* cpPath);
+    void render(World &rWorld);
+    void render(SDL_Texture* pTexture, int dstX = 0, int dstY = 0, int srcX = 0, int srcY = 0);
+    void render(Entity &rEntity);
+    void render(Vector2f position, const char* pText, TTF_Font* pFont, SDL_Color color);
+    void renderCenter(Vector2f position, const char* pText, TTF_Font* pFont, SDL_Color color);
     void clear();
     void display();
     void cleanUp();
--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -1,9 +1,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
-#include <iostream>
+#include <cstdio>
 
+#include "Config.hpp"
 #include "RenderWindow.hpp"
+#include "Entity.hpp"
 
 RenderWindow::RenderWindow()
     :mpWindow(nullptr), mpRenderer(nullptr), mBackgroundColor({0, 0, 0})
@@ -59,6 +62,97 @@ SDL_Texture* RenderWindow::pLoadTexture(const char* cpPath)
     }
 
     return pTexture;
+}
+
+void RenderWindow::render(World &rWorld)
+{
+    render(*rWorld.rpGetPlayer());
+}
+
+void RenderWindow::render(SDL_Texture* pTexture, int srcX, int srcY, int dstX, int dstY)
+{
+    SDL_Rect source;
+    source.x = srcX;
+    source.y = srcY;
+    source.w = 64;
+    source.h = 64;
+
+    SDL_Rect destination;
+    destination.x = dstX;
+    destination.y = dstY;
+    destination.w = 64;
+    destination.h = 64;
+
+    SDL_RenderCopy(mpRenderer, pTexture, &source, &destination);
+}
+
+void RenderWindow::render(Entity &rEntity)
+{
+    SDL_Rect source;
+    source.x = rEntity.rGetCurrentFrame().x;
+    source.y = rEntity.rGetCurrentFrame().y;
+    source.w = rEntity.rGetCurrentFrame().w;
+    source.h = rEntity.rGetCurrentFrame().h;
+    
+    SDL_Rect destination;
+    destination.x = rEntity.rGetPosition().mX;
+    destination.y = rEntity.rGetPosition().mY;
+    destination.w = rEntity.rGetCurrentFrame().w;
+    destination.h = rEntity.rGetCurrentFrame().h;
+
+    SDL_RenderCopy(mpRenderer, rEntity.rpGetTexture(), &source, &destination);
+}
+
+void RenderWindow::render(Vector2f position, const char* pText, TTF_Font* pFont, SDL_Color color)
+{
+    SDL_Surface* pSurfaceMessage = TTF_RenderText_Blended(pFont, pText, color);
+    SDL_Texture* pMessage = SDL_CreateTextureFromSurface(mpRenderer,  pSurfaceMessage);
+
+    SDL_Rect source;
+    source.x = 0;
+    source.y = 0;
+    source.w = pSurfaceMessage->w;
+    source.h = pSurfaceMessage->h;
+
+    SDL_Rect destination;
+    destination.x = position.mX;
+    destination.y = position.mY;
+    destination.w = source.w;
+    destination.h = source.h;
+
+    SDL_RenderCopy(mpRenderer, pMessage, &source, &destination);
+    SDL_FreeSurface(pSurfaceMessage);
+}
+
+void RenderWindow::renderCenter(Vector2f position, const char* pText, TTF_Font* pFont, SDL_Color color)
+{
+    SDL_Surface* pSurfaceMessage = TTF_RenderText_Blended(pFont, pText, color);
+    SDL_Texture* pMessage = SDL_CreateTextureFromSurface(mpRenderer, pSurfaceMessage);
+
+    SDL_Rect source;
+    source.x = 0;
+    source.y = 0;
+    source.w = pSurfaceMessage->w;
+    source.h = pSurfaceMessage->h;
+
+    SDL_Rect destination;
+    destination.x = Config::mScreenWidth/2 - source.w/2 + position.mX;
+    destination.y = Config::mScreenHeight/2 - source.h/2 + position.mY;
+    destination.w = source.w;
+    destination.h = source.h;
+
+    SDL_RenderCopy(mpRenderer, pMessage, &source, &destination);
+    SDL_FreeSurface(pSurfaceMessage);
+}
+
+void RenderWindow::display()
+{
+    SDL_RenderPresent(mpRenderer);
+}
+
+void RenderWindow::cleanUp()
+{
+    SDL_DestroyWindow(mpWindow);
 }
 
 void RenderWindow::clear()
